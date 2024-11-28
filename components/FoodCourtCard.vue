956e66745b4539ebfb019cebbfd2a9b740aebf72
09liweis <weisen.li@hotmail.com>
2024-11-28 13:50:01 -0500

    add default image

--- components/FoodCourtCard.vue
+++ components/FoodCourtCard.vue
@@ -6,7 +6,7 @@
     class="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300"
   >
     <NuxtLink :to="`/food-court/${foodCourt.place_id}`">
-      <img :src="foodCourt.image" :alt="foodCourt.name" class="w-full h-48 object-cover" />
+      <img :src="foodCourt.image || 'https://images.unsplash.com/photo-1615719413546-198b25453f85?w=800'" :alt="foodCourt.name" class="w-full h-48 object-cover" />
       <div class="p-4">
         <h2 class="text-xl font-semibold text-gray-900">{{ foodCourt.name }}</h2>
         <p class="text-gray-600 mt-2">{{ foodCourt.address }}</p>
