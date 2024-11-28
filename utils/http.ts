a8de2c3ac576efceb176b6eac29942fa9fb0944f
09liweis <weisen.li@hotmail.com>
2024-11-28 16:23:03 -0500

    update http request with body

--- utils/http.ts
+++ utils/http.ts
@@ -1,7 +1,23 @@
 const BASE_API = 'https://samliweisen.onrender.com/api/'
 
-export async function sendRequest({url}:any) {
-  const response = await fetch(BASE_API+url);
+interface OptProp {
+  method:string,
+  headers:any,
+  body?:any
+}
+
+export async function sendRequest({url,method='GET',body={}}:any) {
+  const opt:OptProp = {
+    method,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  };
+  if (Object.keys(body).length) {
+    opt.body = JSON.stringify(body);
+  }
+  const response = await fetch(BASE_API+url, opt);
   const data = await response.json();
   return data;
 }
